90f09834e38f1efc930c44986d63f8492e933a7b
Tania Tedla <124741266+nataniatedla@users.noreply.github.com>
2023-07-29 17:28:21 -0400

    Add files via upload

--- milestone4/show-wishlist.php
+++ milestone4/show-wishlist.php
@@ -1,6 +1,18 @@
 <?php
     session_start();
+    
+    $servername = "localhost";
+    $username = "kajibade1";
+    $password = "kajibade1";
+    $dbname = "kajibade1";
 
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password , $dbname);
+
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
 
 <!DOCTYPE html>
@@ -22,62 +34,80 @@
                 </div>
             </header>
 
+        <div class="container text-white">
+    <h2 class='text-center text-white'>My Wishlist</h2>
 
-            <div class="block">
-                <h2>My Wishlist</h2>
+    <section id="content">
+		<div class="content-blog content-account">
+			<div class="container">
+				<div class="row">
+				 
+					<div class="col-md-12">
 
-                <section id="content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <br>
-                                <table class="wishlist">
-                                    <thead>
-                                        <tr>
-                                            <th>Property Name</th>
-                                            <th>Price</th>
-
-                                            <th>Date and Time</th>
-                                        </tr>
-                                    </thead>
-                                <tbody>
-                                <?php
-                                   $u_id = $_SESSION['user'];
-
-                                   $sql = "SELECT * FROM wishlist JOIN properties.property_id = wishlist.pid";
-                                   //$sql = "SELECT * FROM wishlist JOIN props on props.home uid = wishlist.pid";
-                                   $result = mysqli_query($conn, $sqli);
-
-                                   if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                ?>
-
-                                <tr>
-                                    <td>
-                                        <a href="houseinfo.php?id=<?php echo $row["property_id"] ?>">	<?php echo $row["address"] ?></a>
-                                    </td>
-                                    
-                                    <td>
-                                        <?php echo $row["price"] ?>
-                                    </td>
-                                </tr> 
-                            <?php
-				                }
-                                } else {
-                                    echo "0 results";
-                                } 
-                
-                            ?>
-                        </tbody>
-                        </table>
+		 
+			<br>
+			<table class="cart-table account-table table table-bordered bg-white text-dark">
+				<thead>
+					<tr>
+						<th>Property Name</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+				$u_id = $_SESSION['user_name']; 
+  
+				$sql = "SELECT * FROM wishlist WHERE uid = '$u_id'";
+				//$sql = "SELECT * FROM wishlist JOIN products on products.product_id=wishlist.pid";
+				$result = mysqli_query($conn, $sql);
+			  
+				if (mysqli_num_rows($result) > 0) {
+				 // output data of each row
+				 while($row = mysqli_fetch_assoc($result)) {
+ 			?>
+					<tr>
+						<td>
+                        <a href="houseinfo.php?id=<?php echo $row["property_id"] ?>">	<?php echo $row["address"] ?></a>
+						
+						</td>
+						<td>
+						<?php echo $row["price"] ?>
+						</td>
+					 
+					</tr>
+				 
+			
+			<?php
+				}
+			   } else {
+				 echo "0 results";
+			   }
+			 
+			 
+			 ?>
 
 
-                            </div>
-                        </div>
-                    </div>
-                </section>
 
-            </div>
+
+				
+				</tbody>
+			</table>		
+
+		 
+ 
+
+
+
+			</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+ 
+</div>
 
     </body>
 </html>
--- milestone4/wishlist.php
+++ milestone4/wishlist.php
@@ -1,7 +1,19 @@
 <?php
 
     session_start();
-    // include("database.php");
+    
+    $servername = "localhost";
+    $username = "kajibade1";
+    $password = "kajibade1";
+    $dbname = "kajibade1";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password , $dbname);
+
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
 
     if (!isset($_SESSION['user_name'])) {
         header('location:dashboardNew.php');
@@ -11,7 +23,7 @@
         $p_id = $_GET['id']; 
 
         
-        $sql_Check = "SELECT * FROM wishlist WHERE pid = $p_id AND uid = $u_id";
+        $sql_Check = "SELECT * FROM wishlist where pid = $p_id AND uid = $u_id";
         $result_check = mysqli_query($conn, $sql_Check);
 
         if (mysqli_num_rows($result_check) == 1 ) {
@@ -19,15 +31,16 @@
             header('location:show-wishlist.php');
         } else {
 
-        }
-
-        $insertWishlist = "INSERT INTO wishlist (pid, uid)
+            $insertWishlist = "INSERT INTO wishlist (pid, uid)
             VALUES ('$p_id', '$u_id')";
 
-        if (mysqli_query($conn, $insertWishlist)) {
-            echo "inserted";
-            header('location:show-wishlist.php');
+            if (mysqli_query($conn, $insertWishlist)) {
+                echo "inserted";
+                header('location:show-wishlist.php');
+            }
         }
+
+        
     }
 
 
